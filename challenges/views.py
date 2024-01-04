c9734445d90d568835087a59c25168665df73e73
Siddharth CV <siddharthcv@Siddharths-Air.lan>
2024-01-04 00:38:45 -0500

    section 3: urls and views

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
+
+monthly_challenges = {
+    "january": "Eat no meat for the entire month",
+    "february": "Walk for atleast 20 minutes every day",
+    "march": "Learn Django everyday",
+    "april": "Eat no meat for the entire month",
+    "may": "Walk for atleast 20 minutes every day",
+    "june": "Learn Django everyday",
+    "july": "Eat no meat for the entire month",
+    "august": "Walk for atleast 20 minutes every day",
+    "september": "Learn Django everyday",
+    "october": "Eat no meat for the entire month",
+    "november": "Walk for atleast 20 minutes every day",
+    "december": "Learn Django everyday"
+}
 
 # Create your views here.
+
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"        
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("<h1>Invalid Month!</h1>")
+    redirect_month = months[month-1]
+    redirect_path = reverse("month-challenge", args=[redirect_month]) #/challenge/january
+    return HttpResponseRedirect(redirect_path)
+
+def monthly_challenge(request,month):
+    try:
+        challenge_text = monthly_challenges[month]
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
+    except:
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
